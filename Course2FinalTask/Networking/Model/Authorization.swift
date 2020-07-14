982fc2b406634ef7384ecb8f4bae27be378524de
Oleksii Bardin <bardincom@icloud.com>
2020-07-14 12:43:36 +0300

    INST-7_Final_changes Преобразование json используя Encodable

---
+++ Course2FinalTask/Networking/Model/Authorization.swift
@@ -0,0 +1,14 @@
+//
+//  Authorization.swift
+//  Course3FinalTask
+//
+//  Created by Aleksey Bardin on 14.07.2020.
+//  Copyright © 2020 Bardincom. All rights reserved.
+//
+
+import Foundation
+
+struct Authorization: Encodable {
+  let login: String
+  let password: String
+}
---
+++ Course2FinalTask/Networking/Model/UserIDRequest.swift
@@ -0,0 +1,13 @@
+//
+//  UserIDRequest.swift
+//  Course3FinalTask
+//
+//  Created by Aleksey Bardin on 14.07.2020.
+//  Copyright © 2020 Bardincom. All rights reserved.
+//
+
+import Foundation
+
+struct UserIDRequest: Encodable {
+  let userID: String
+}
